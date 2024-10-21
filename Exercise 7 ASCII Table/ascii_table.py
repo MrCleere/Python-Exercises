913e50979efd354084a865738c2b09281c6a4402
MrCleere <162706923+MrCleere@users.noreply.github.com>
2024-10-21 10:40:26 +0100

    Delete Exercise 7 ASCII Table/ascii_table.py

--- Exercise 7 ASCII Table/ascii_table.py
+++
@@ -1,9 +0,0 @@
-def printASCIITable():
-    for i in range(32, 127):
-        # Get the ASCII character corresponding to the ASCII value
-        char = chr(i)
-        # Print the ASCII value followed by the character
-        print(f"{i} {char}")
-
-# Call the function to display the ASCII table
-printASCIITable()
